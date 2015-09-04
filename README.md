6210dba95ab999bfe49c828c0140e12026557e89
Siddharth Swaminathan <siddharth.swaminathan@daimler.com>
2015-09-03 22:26:10 -0700

    quit if settings is not found

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gstylemailer
 Parses the google style guide, and sends you a daily dose. :)
+
+If you want to host this on your own server, make a copy of settings.py.template and fill in the proper values.
+
+Overview:
+`data/mail_cache.p` is the database where information on who has been mailed which snippet is saved.
+
+`mailing_list.txt` : one email per line.
--- mailer.py
+++ mailer.py
@@ -1,18 +1,20 @@
 import cPickle as pickle
-import os
+import os, sys
 import random
 from parse_styles import GStyleGuide, GStyleGuideParser
 
+try:
+	from settings import *
+except:
+	print 'Cannot find the settings file. Quitting.'
+	sys.exit(1)
 
-# Import smtplib for the actual sending function
+# email related imports
 import smtplib
-
-# Import the email modules we'll need
 from email.mime.text import MIMEText
 
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-server = smtplib.SMTP('localhost', 1025)
+# Send the message via our own SMTP server.
+server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
 
 def mail_item(mail_id, item, item_num):
 	# render the item with styles
@@ -20,18 +22,20 @@ def mail_item(mail_id, item, item_num):
 	msg_body = item.body
 	msg_body += '\n<link rel="stylesheet" type="text/css" href="https://google-styleguide.googlecode.com/svn/trunk/styleguide.css">'
 	msg = MIMEText(msg_body, 'html')
-	me = 'gstyleguidemailer@siddharthswaminathan.in'
+	me = MAILER_FROM
 	# you == the recipient's email address
 	msg['Subject'] = '[GStyleGuideMailer] Snippet #%d' % (item_num+1)
 	msg['From'] = me
 	msg['To'] = mail_id
-	server.sendmail(me, [mail_id], msg.as_string())
-
+	try:
+		server.sendmail(me, [mail_id], msg.as_string())
+		return True
+	except:
+		return False
 
 
 STYLES_FILENAME = 'data/parsed_styles.p'
 MAIL_CACHE = 'data/mail_cache.p'
-MAILING_LIST = 'mailing_list.txt'
 
 # if parsed file exists, load them. If not, invoke the parser.
 parsed_styles = None
@@ -52,22 +56,26 @@ with open(MAILING_LIST, 'rb') as mlistfile:
 	mailing_list = mlistfile.readlines()
 	mailing_list = [m.strip() for m in mailing_list]
 
-# create a list of emails to send the mails to
+# create a dict {mail_id : [items_mailed]}
 mail_cache = {m:[] for m in mailing_list}
+
+# Update from cache
 if os.path.exists(MAIL_CACHE):
 	mail_cache_saved = pickle.load(open(MAIL_CACHE, 'rb'))
+	# filter out items that are no longer in the mailing_list
+	mail_cache_saved = {k:v for (k,v) in mail_cache_saved.iteritems() if k in mail_cache}
+	# update our current list with the cache
 	mail_cache.update(mail_cache_saved)
 
 # print mail_cache
-
 master_set = set(range(NUM_STYLES))
-
 for (mail_id, mailed_items) in mail_cache.iteritems():
 	# print mail_id, mailed_items
 	allowed_set = master_set - set(mailed_items)
 	selected_item = random.sample(allowed_set, 1)[0]
-	mail_item(mail_id, parsed_styles[selected_item], len(mailed_items))
-	mail_cache[mail_id].append(selected_item)
+	success = mail_item(mail_id, parsed_styles[selected_item], len(mailed_items))
+	if success:
+		mail_cache[mail_id].append(selected_item)
 
 # save mail_cache
 pickle.dump(mail_cache, open(MAIL_CACHE, 'w'))
